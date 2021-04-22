093079003a13c4e31c029e3ead034a082a6347c0
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-04-22 12:34:52 -0700

    updated schema on model and exports set up, along with mongo

--- models/workout.js
+++ models/workout.js
@@ -1,2 +1,38 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: { type: Date, required: true },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Type of exercise required"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Name of exercise required"
+        },
+        duration: {
+            type: Number,
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance: {
+            type: Number,
+        },
+    }]
+
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
